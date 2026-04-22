690ce5e97e3f895bcd96d097ea190b2b9ac04b72
Isa Maharramov <isa123meherremov@gmail.com>
2026-04-22 04:17:08 +0400

    4:17

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -1,19 +1,13 @@
 #!/usr/bin/env python3
-"""
-Module to calculate the sum of i squared
-"""
+"""function that sum powers based on Faulhaber's Formula"""
 
 
 def summation_i_squared(n):
-    """
-    Calculates the sum of i^2 from 1 to n
-    """
-    # Check if n is specifically an integer type
+    """m(m + 1)(2m + 1) / 6"""
+
     if type(n) is not int:
         return None
-    if n < 0:
+    elif n < 1:
         return None
-
-    # Square pyramidal number formula: n(n + 1)(2n + 1) / 6
-    # Integer division // ensures the return is an integer
-    return (n * (n + 1) * (2 * n + 1)) // 6
+    else:
+        return int((n * (1 + n)) * (2 * n + 1) / 6)
