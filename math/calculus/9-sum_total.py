5d405de833ba34aca81ad54c39e089524cf6e31b
Isa Maharramov <isa123meherremov@gmail.com>
2026-04-22 04:11:47 +0400

    4:11

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -8,11 +8,12 @@ def summation_i_squared(n):
     """
     Calculates the sum of i^2 from 1 to n
     """
+    # Strict integer check: if n is not an int, it's not a 'valid number'
     if not isinstance(n, int):
         return None
     if n < 0:
         return None
 
-    # Square pyramidal number formula
-    # Result must be an integer
+    # Using the square pyramidal number formula: n(n+1)(2n+1)/6
+    # Integer division // ensures we return an int type
     return (n * (n + 1) * (2 * n + 1)) // 6
