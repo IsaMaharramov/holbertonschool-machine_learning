d45715bc95bbd7153cf65be2dd1f0db979b9b095
Isa Maharramov <seferovaqelem1963@gmail.com>
2026-05-11 18:30:54 +0400

    6:30

---
+++ math/bayesian_prob/0-likelihood.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""
+Contains the likelihood function for Bayesian probability
+"""
+import numpy as np
+
+
+def likelihood(x, n, P):
+    """
+    Calculates the likelihood of obtaining data given hypothetical probabilities
+    """
+    if not isinstance(n, int) or n <= 0:
+        raise ValueError("n must be a positive integer")
+    if not isinstance(x, int) or x < 0:
+        raise ValueError(
+            "x must be an integer that is greater than or equal to 0")
+    if x > n:
+        raise ValueError("x cannot be greater than n")
+    if not isinstance(P, np.ndarray) or len(P.shape) != 1:
+        raise TypeError("P must be a 1D numpy.ndarray")
+    if not np.all((P >= 0) & (P <= 1)):
+        raise ValueError("All values in P must be in the range [0, 1]")
+
+    # Combinations nCr calculation
+    def factorial(n):
+        res = 1
+        for i in range(1, n + 1):
+            res *= i
+        return res
+
+    n_fact = factorial(n)
+    x_fact = factorial(x)
+    nx_fact = factorial(n - x)
+    n_cr = n_fact / (x_fact * nx_fact)
+
+    # Binomial distribution: nCr * p^x * (1-p)^(n-x)
+    l_hood = n_cr * (P ** x) * ((1 - P) ** (n - x))
+
+    return l_hood
---
+++ math/bayesian_prob/README.md
@@ -0,0 +1,11 @@
+* [Bayesian probability](https://en.wikipedia.org/wiki/Bayesian_probability)
+
+* [Bayesian statistics](https://en.wikipedia.org/wiki/Bayesian_statistics)
+
+* [Bayes' Theorem - The Simplest Case](https://youtu.be/XQoLVl31ZfQ?si=l1kzYMTzycptBTWW)
+
+* [A visual guide to Bayesian thinking](https://youtu.be/BrK7X_XlGB8?si=Q0zulDXa-8PEZWPe)
+
+* [Base Rates](https://onlinestatbook.com/2/probability/base_rates.html)
+
+* [Bayesian statistics: a comprehensive course](https://youtube.com/playlist?list=PLFDbGp5YzjqXQ4oE4w9GVWdiokWB9gEpm&si=iqiqrTp6GXullm0E)
