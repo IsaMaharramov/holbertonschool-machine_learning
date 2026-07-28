9f9a3ce929363e44be06c53e82156ee593acb717
Isa Maharramov <seferovaqelem1963@gmail.com>
2026-07-29 00:17:51 +0400

    12:17

--- pipeline/data_augmentation/100-pca.py
+++ pipeline/data_augmentation/100-pca.py
@@ -8,43 +8,42 @@ def pca_color(image, alphas):
 
     Args:
         image: a 3D tf.Tensor containing the image to change.
-        alphas: a tuple of length 3 containing the amount each channel
+        alphas: a tuple of length 3 containing the amount that each channel
                 should change.
 
     Returns:
         The augmented image tensor.
     """
-    orig_dtype = image.dtype
-    img_float = tf.cast(image, tf.float32)
+    # Cast the image to float32 for calculations
+    img = tf.cast(image, tf.float32)
 
-    # Reshape image pixels to (N, 3)
-    reshaped_img = tf.reshape(img_float, [-1, 3])
+    # Flatten the image to 2D array (num_pixels, 3)
+    flat_img = tf.reshape(img, [-1, 3])
 
-    # Mean subtract across channels
-    mean = tf.reduce_mean(reshaped_img, axis=0)
-    centered = reshaped_img - mean
+    # Mean center the pixels across the channel axis
+    mean = tf.reduce_mean(flat_img, axis=0)
+    centered = flat_img - mean
 
-    # Compute 3x3 covariance matrix over channels
-    num_pixels = tf.cast(tf.shape(reshaped_img)[0], tf.float32)
-    cov = tf.matmul(centered, centered, transpose_a=True) / num_pixels
+    # Compute the 3x3 covariance matrix
+    num_pixels = tf.cast(tf.shape(centered)[0] - 1, tf.float32)
+    cov = tf.tensordot(tf.transpose(centered), centered, axes=1) / num_pixels
 
-    # Compute eigenvalues and eigenvectors
-    e_val, e_vec = tf.linalg.eigh(cov)
+    # Perform eigendecomposition to get eigenvalues and eigenvectors
+    eigen_values, eigen_vectors = tf.linalg.eigh(cov)
 
-    # Format alphas into tensor shape (3, 1)
-    alphas_tensor = tf.cast(alphas, tf.float32)
-    alphas_tensor = tf.reshape(alphas_tensor, (3, 1))
+    # Convert the alphas tuple to a float32 tensor
+    alphas = tf.convert_to_tensor(alphas, dtype=tf.float32)
 
-    # e_val needs to be (3, 1) to multiply element-wise with alphas
-    e_val = tf.reshape(e_val, (3, 1))
+    # Compute the perturbation delta
+    # Broadcast element-wise multiplication on eigenvectors, then sum across axis 1
+    # This exactly mimics [p1, p2, p3] @ [a1*L1, a2*L2, a3*L3]^T
+    delta = tf.reduce_sum(eigen_vectors * (alphas * eigen_values), axis=1)
 
-    # Linear combination: p_i * alpha_i * lambda_i
-    # e_vec @ (alphas * e_val)
-    delta = tf.matmul(e_vec, alphas_tensor * e_val)
-    delta = tf.reshape(delta, (1, 1, 3))
+    # Add the perturbation vector directly to the image
+    pca_img = img + delta
 
-    # Add perturbation, clip to valid RGB pixel range [0, 255]
-    augmented = img_float + delta
-    augmented = tf.clip_by_value(augmented, 0.0, 255.0)
+    # Clip the pixel values to the valid [0, 255] range
+    pca_img = tf.clip_by_value(pca_img, 0.0, 255.0)
 
-    return tf.cast(augmented, orig_dtype)
+    # Explicitly cast and return as uint8
+    return tf.cast(pca_img, tf.uint8)
