5fa466e5765c37b97ca4fac26dc11ce9ae7396f9
Isa Maharramov <seferovaqelem1963@gmail.com>
2026-07-29 00:27:10 +0400

    12:27

--- pipeline/data_augmentation/100-pca.py
+++ pipeline/data_augmentation/100-pca.py
@@ -1,69 +1,53 @@
 #!/usr/bin/env python3
-
+"""Module for PCA color augmentation."""
 import tensorflow as tf
-import numpy as np
 
 
 def pca_color(image, alphas):
-    """
-    pca color augmentation based on alex-net paper
+    """Performs PCA color augmentation as described in the AlexNet paper.
+
     Args:
-        image: 3D tf.Tensor containing the image to change
-        alphas: tuple of length 3 containing the amount
-                that each channel should change
-    Returns: Returns the augmented image
+        image: a 3D tf.Tensor containing the image to change.
+        alphas: a tuple of length 3 containing the amount that each channel
+                should change.
+
+    Returns:
+        The augmented image tensor.
     """
-    # getting the original image from tf
-    original_image = tf.keras.preprocessing.image.img_to_array(image)
-    cp_original = original_image.astype(float).copy()
+    # Preserve original dtype to cast back later
+    orig_dtype = image.dtype
+    
+    # Cast the image to float32 for calculations
+    img = tf.cast(image, tf.float32)
 
+    # Flatten the image to 2D array (num_pixels, 3)
+    flat_img = tf.reshape(img, [-1, 3])
 
-    # flatten image to columns of RGB
-    original_image = original_image / 255.0
-    img_rs = original_image.reshape(-1, 3)
+    # Mean center the pixels across the channel axis
+    mean = tf.reduce_mean(flat_img, axis=0)
+    centered = flat_img - mean
 
-    # center mean
-    img_centered = img_rs - np.mean(img_rs, axis=0)
+    # Compute the 3x3 covariance matrix using TensorFlow
+    # Normalized by N-1
+    num_pixels = tf.cast(tf.shape(centered)[0] - 1, tf.float32)
+    cov = tf.tensordot(tf.transpose(centered), centered, axes=1) / num_pixels
 
-    # paper says 3x3 covariance matrix
-    img_cov = np.cov(img_centered, rowvar=False)
+    # Perform eigendecomposition using TensorFlow
+    eigen_values, eigen_vectors = tf.linalg.eigh(cov)
 
-    # eigen values and eigen vectors
-    eig_vals, eig_vecs = np.linalg.eigh(img_cov)
+    # Convert the alphas tuple to a float32 tensor
+    alphas = tf.convert_to_tensor(alphas, dtype=tf.float32)
 
+    # Compute the perturbation delta
+    # Broadcast element-wise multiplication on eigenvectors, sum across axis 1
+    # This mimics [p1, p2, p3] @ [a1*L1, a2*L2, a3*L3]^T purely in TF
+    delta = tf.reduce_sum(eigen_vectors * (alphas * eigen_values), axis=1)
 
-    # sort values and vector
-    sort_perm = eig_vals[::-1].argsort()
-    eig_vals[::-1].sort()
-    eig_vecs = eig_vecs[:, sort_perm]
+    # Add the perturbation vector directly to the image
+    pca_img = img + delta
 
-    # get [p1, p2, p3]
-    m1 = np.column_stack((eig_vecs))
+    # Clip the pixel values to the valid [0, 255] range
+    pca_img = tf.clip_by_value(pca_img, 0.0, 255.0)
 
-    # get 3x1 matrix of eigen values multiplied by random variable draw from normal
-    # distribution with mean of 0 and standard deviation of 0.1
-    m2 = np.zeros((3, 1))
-
-    # broad cast to speed things up
-    m2[:, 0] = alphas * eig_vals[:]
-
-    # this is the vector that we're going to add to each pixel in a moment
-    add_vect = np.matrix(m1) * np.matrix(m2)
-
-    # RGB
-    for idx in range(3):
-        cp_original[..., idx] += add_vect[idx]
-
-    # for image processing it was found that working with float 0.0 to 1.0
-    # was easier than integers between 0-255
-    # cp_original /= 255.0
-    cp_original = np.clip(cp_original, 0.0, 255.0)
-
-    # cp_original *= 255
-    cp_original = cp_original.astype(np.uint8)
-
-    # about 100x faster after vectorizing the numpy, it will be even faster later
-    # since currently it's working on full size images and not small, square
-    # images that will be fed in later as part of the post processing before being
-    # sent into the model
-    return cp_original
+    # Explicitly cast back to original data type (e.g., uint8)
+    return tf.cast(pca_img, orig_dtype)
